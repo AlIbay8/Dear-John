9036ed2a7974c21804c6ac0caf15c14af5bb949d
AlIbay8 <43456952+AlIbay8@users.noreply.github.com>
2018-12-12 18:49:30 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,5 @@
+$("button").click(function() {
+    var phrase = $("input").val();
+    if (phrase === "1815") {
+    $(".riddle").text("My first is in sentience but not in instinct. My second is in axletree but not in relater. My third is in pertain but not in retina. My fourth is in annalist but not in satanist. My fifth is in contrite but not in inciter. My sixth is in repeat but not in peat. My seventh is in instantiate but not in instant.");}
+});
